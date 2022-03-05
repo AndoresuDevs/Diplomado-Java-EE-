177fa185ef8e3ae1df4d09822ddda756800f0a1a
Andres Dimas <andres.dimas@milenio.com>
2022-03-05 14:15:44 -0600

    configuracion inicial de hibernate sin cambios al codigo

--- Libreria v01/src/main/java/acciones/EditarProveedorAccion.java
+++ Libreria v01/src/main/java/acciones/EditarProveedorAccion.java
@@ -11,6 +11,7 @@ public class EditarProveedorAccion extends Accion{
 	@Override
 	public String ejecutar(HttpServletRequest request, HttpServletResponse response) {
 		// TODO Auto-generated method stub
+		System.out.println("OTRO ID EN ACCION ES:"+request.getParameter("idProv"));
 				int id = Integer.parseInt(request.getParameter("idProv"));
 				String nom = request.getParameter("nomProv");
 				String tel = request.getParameter("telProv");
